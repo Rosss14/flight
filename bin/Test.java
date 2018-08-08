477389563052d2393a737f9a9e34301221976890
Rosss14 <41752855+Rosss14@users.noreply.github.com>
2018-08-08 14:01:14 +0300

    Add files via upload

---
+++ bin/Test.java
@@ -0,0 +1,27 @@
+public class Test{
+	static Flight fl1 = new Flight("LH1523");
+	static Flight fl2 = new Flight("LH1523");
+	static Flight fl3 = new Flight("RA1324");
+
+	public static void setAirports(String []args) {
+		fl1.setOrigin("Munich");
+		fl1.setDestination("Frankfurt");
+		fl2.setOrigin("Munich");
+		fl2.setDestination("Frankfurt");
+		fl3.setOrigin("Dublin");
+		fl3.setDestination("Stockholm");
+	}
+	
+	public static void testComparison(String []args) {
+		System.out.println(fl1.equals1(fl2));
+		System.out.println(fl1.equals1(fl3));
+		System.out.println(fl2.equals1(fl2));
+		System.out.println(fl1.equals(fl2));
+		System.out.println(fl1.equals(fl3));
+		System.out.println(fl2.equals(fl2));
+	}
+	public static void main(String []args) {
+		setAirports(args);
+		testComparison(args);
+	}
+}
