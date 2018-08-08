10adf3c50ab862b43c46be71cb5c48c48e1bb793
Rosss14 <41752855+Rosss14@users.noreply.github.com>
2018-08-08 14:03:08 +0300

    Add files via upload

--- src/Test.java
+++ src/Test.java
@@ -1,10 +1,9 @@
-
-public class Test {
+public class Test{
 	static Flight fl1 = new Flight("LH1523");
 	static Flight fl2 = new Flight("LH1523");
 	static Flight fl3 = new Flight("RA1324");
-	
-	public static void main1(String []args) {
+
+	public static void setAirports(String []args) {
 		fl1.setOrigin("Munich");
 		fl1.setDestination("Frankfurt");
 		fl2.setOrigin("Munich");
@@ -13,16 +12,16 @@ public class Test {
 		fl3.setDestination("Stockholm");
 	}
 	
-	public static void main(String []args) {
+	public static void testComparison(String []args) {
 		System.out.println(fl1.equals1(fl2));
 		System.out.println(fl1.equals1(fl3));
 		System.out.println(fl2.equals1(fl2));
-	}
-	public static void main11(String []args) {
 		System.out.println(fl1.equals(fl2));
 		System.out.println(fl1.equals(fl3));
 		System.out.println(fl2.equals(fl2));
-		
 	}
-
+	public static void main(String []args) {
+		setAirports(args);
+		testComparison(args);
+	}
 }
