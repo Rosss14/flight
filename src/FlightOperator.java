490c188a1d0208fbbb4419b07faf3f95d9cf6eb9
Rosss14 <41752855+Rosss14@users.noreply.github.com>
2018-08-09 20:16:03 +0300

    Add files via upload

--- src/FlightOperator.java
+++ src/FlightOperator.java
@@ -1,14 +1,33 @@
 
 public class FlightOperator {
-	static Flight[] flightArray = Ght.generateFlights();
-	public static Flight[] sortFlights() {
-		Flight fl0 = flightArray[0];
+	public Flight[] sortFlights() {
+		Flight[] flightArray = Ght.generateFlights();
+		String fl0 = flightArray[0].toString();
 		for (int i = 0; i < flightArray.length; i ++) {
-			if (flightArray[i].toString().compareToIgnoreCase(fl0.toString())<=0) {
-				flightArray[?]=?; //Не знаю як тут зробити.
+			if (flightArray[i].toString().compareToIgnoreCase(fl0)<0) {
+				fl0=flightArray[i].toString();
+				flightArray[0]=flightArray[i];
 			}
-			if (flightArray[i].toString().compareToIgnoreCase(fl0.toString())>=0) {
-				flightArray[?]=?;//І тут теж.
+		}
+		String fl1 = flightArray[1].toString();
+		for (int i = 1; i < flightArray.length; i ++) {
+			if (flightArray[i].toString().compareToIgnoreCase(fl1)<0) {
+				fl1=flightArray[i].toString();
+				flightArray[1]=flightArray[i];
+			}
+		}
+		String fl2 = flightArray[2].toString();
+		for (int i = 2; i < flightArray.length; i ++) {
+			if (flightArray[i].toString().compareToIgnoreCase(fl2)<0) {
+				fl2=flightArray[i].toString();
+				flightArray[2]=flightArray[i];
+			}
+		}
+		String fl3 = flightArray[3].toString();
+		for (int i = 3; i < flightArray.length; i ++) {
+			if (flightArray[i].toString().compareToIgnoreCase(fl3)<0) {
+				fl3=flightArray[i].toString();
+				flightArray[3]=flightArray[i];
 			}
 		}
 		return flightArray;
