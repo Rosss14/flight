685a403fd74fe6ebd59b55fdc9f7fb52a743fc78
matvii.kovtun <kovtun_m@ucu.edu.ua>
2018-08-10 12:15:29 +0300

    Refactored sort method, changed class architecture a bit

--- src/FlightOperator.java
+++ src/FlightOperator.java
@@ -1,35 +1,32 @@
 
 public class FlightOperator {
-	public Flight[] sortFlights() {
-		Flight[] flightArray = Ght.generateFlights();
-		String fl0 = flightArray[0].toString();
-		for (int i = 0; i < flightArray.length; i ++) {
-			if (flightArray[i].toString().compareToIgnoreCase(fl0)<0) {
-				fl0=flightArray[i].toString();
-				flightArray[0]=flightArray[i];
-			}
-		}
-		String fl1 = flightArray[1].toString();
-		for (int i = 1; i < flightArray.length; i ++) {
-			if (flightArray[i].toString().compareToIgnoreCase(fl1)<0) {
-				fl1=flightArray[i].toString();
-				flightArray[1]=flightArray[i];
-			}
-		}
-		String fl2 = flightArray[2].toString();
-		for (int i = 2; i < flightArray.length; i ++) {
-			if (flightArray[i].toString().compareToIgnoreCase(fl2)<0) {
-				fl2=flightArray[i].toString();
-				flightArray[2]=flightArray[i];
-			}
-		}
-		String fl3 = flightArray[3].toString();
-		for (int i = 3; i < flightArray.length; i ++) {
-			if (flightArray[i].toString().compareToIgnoreCase(fl3)<0) {
-				fl3=flightArray[i].toString();
-				flightArray[3]=flightArray[i];
-			}
-		}
-		return flightArray;
-	}
+    Flight[] flights;
+
+    public FlightOperator(Flight[] flights) {
+        this.flights = flights;
+    }
+
+
+    public void sortFlights() {
+        // TODO: add sort by different criteria if code is same
+        for (int i = 0; i < this.flights.length; ++i) {
+            for (int j = i + 1; j < this.flights.length; ++j) {
+                if (this.flights[i].getCode().compareTo(this.flights[j].getCode()) > 0) {
+                    Flight temp = this.flights[i];
+                    this.flights[i] = this.flights[j];
+                    this.flights[j] = temp;
+                }
+            }
+        }
+
+    }
+
+    public String toString() {
+        String print = "";
+        for (int i = 0; i < this.flights.length; ++i) {
+            print += this.flights[i].toString();
+            print += " -> ";
+        }
+        return print;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test{
 	static Flight fl2 = new Flight("Lh1523");
 	static Flight fl3 = new Flight("RA1324");
 
-	public static void setAirports(String []args) {
+	public static void setAirports() {
 		fl1.setOrigin("Munich");
 		fl1.setDestination("Frankfurt");
 		fl2.setOrigin("Munich");
@@ -12,7 +12,7 @@ public class Test{
 		fl3.setDestination("StockHolm");
 	}
 	
-	public static void testComparison(String []args) {
+	public static void testComparison() {
 		System.out.println(fl1.equals1(fl2));
 		System.out.println(fl1.equals1(fl3));
 		System.out.println(fl2.equals1(fl2));
@@ -20,8 +20,20 @@ public class Test{
 		System.out.println(fl1.equals(fl3));
 		System.out.println(fl2.equals(fl2));
 	}
+
+	public static void testSort(FlightOperator fo){
+		System.out.println(fo);
+		fo.sortFlights();
+		System.out.println("Sorted by code");
+		System.out.println(fo);
+
+	}
+
 	public static void main(String []args) {
-		setAirports(args);
-		testComparison(args);
+//		setAirports();
+//		testComparison();
+		testSort(new FlightOperator(new Flight[]{fl3, fl1, fl2}));
+
+
 	}
 }
