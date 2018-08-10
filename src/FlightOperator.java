ab4839d8bff6d57a2ac6fa8cf95e199a32acd546
Rosss14 <41752855+Rosss14@users.noreply.github.com>
2018-08-10 14:42:42 +0300

    Add files via upload

--- src/FlightOperator.java
+++ src/FlightOperator.java
@@ -11,10 +11,24 @@ public class FlightOperator {
         // TODO: add sort by different criteria if code is same
         for (int i = 0; i < this.flights.length; ++i) {
             for (int j = i + 1; j < this.flights.length; ++j) {
-                if (this.flights[i].getCode().compareTo(this.flights[j].getCode()) > 0) {
-                    Flight temp = this.flights[i];
+            	if (this.flights[i].getOrigin().compareToIgnoreCase(this.flights[j].getOrigin())>0) {
+            		Flight temp = this.flights[i];
                     this.flights[i] = this.flights[j];
                     this.flights[j] = temp;
+            	}
+            	else if (this.flights[i].getOrigin().compareToIgnoreCase(this.flights[j].getOrigin())==0) {
+            		if (this.flights[i].getDestination().compareToIgnoreCase(this.flights[j].getDestination())>0) {
+            			Flight temp = this.flights[i];
+                        this.flights[i] = this.flights[j];
+                        this.flights[j] = temp;
+            		}
+            		else if (this.flights[i].getDestination().compareToIgnoreCase(this.flights[j].getDestination())==0) {
+            			if (this.flights[i].getCode().compareToIgnoreCase(this.flights[j].getCode()) > 0) {
+                			Flight temp = this.flights[i];
+                			this.flights[i] = this.flights[j];
+                			this.flights[j] = temp;
+            		}
+            	}
                 }
             }
         }
