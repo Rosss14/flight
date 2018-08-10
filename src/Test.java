4fe26f4aab7e33ccaa38c96ccf54776115922a28
Rosss14 <41752855+Rosss14@users.noreply.github.com>
2018-08-10 14:51:05 +0300

    Add files via upload

--- src/Test.java
+++ src/Test.java
@@ -2,38 +2,35 @@ public class Test{
 	static Flight fl1 = new Flight("LH1523");
 	static Flight fl2 = new Flight("Lh1523");
 	static Flight fl3 = new Flight("RA1324");
-
-	public static void setAirports() {
+	static Hora hora1 = new Hora(14 , 30);
+	static FlightOperator fo = new FlightOperator (Ght.generateFlights());
+	public static void setAirports(String []args) {
 		fl1.setOrigin("Munich");
 		fl1.setDestination("Frankfurt");
 		fl2.setOrigin("Munich");
 		fl2.setDestination("FRankfurt");
 		fl3.setOrigin("Dublin");
 		fl3.setDestination("StockHolm");
+		fl1.setAirLine("LuftHansa");
+		fl1.setDuration(35);
+		fl1.setHour(hora1);
 	}
 	
-	public static void testComparison() {
+	public static void testComparison(String []args) {
 		System.out.println(fl1.equals1(fl2));
 		System.out.println(fl1.equals1(fl3));
 		System.out.println(fl2.equals1(fl2));
 		System.out.println(fl1.equals(fl2));
 		System.out.println(fl1.equals(fl3));
 		System.out.println(fl2.equals(fl2));
+		System.out.println(fl1.textRepresentation());
 	}
-
-	public static void testSort(FlightOperator fo){
-		System.out.println(fo);
-		fo.sortFlights();
-		System.out.println("Sorted by code");
-		System.out.println(fo);
-
-	}
+	
 
 	public static void main(String []args) {
-//		setAirports();
-//		testComparison();
-		testSort(new FlightOperator(new Flight[]{fl3, fl1, fl2}));
-
-
+		setAirports(args);
+		fo.sortFlights();
+		System.out.println(fo.toString());
+		
 	}
 }
